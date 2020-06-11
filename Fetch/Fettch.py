ad7bddeee48a339acd80ff96978fb29ffaa1b6eb
Mohammad Shughri <48556029+MXS11@users.noreply.github.com>
2020-06-11 08:00:16 +0300

    Delete Fettch.py

--- Fetch/Fettch.py
+++
@@ -1,7 +0,0 @@
-from firebase import firebase
-
-firebase = firebase.FirebaseApplication('https://iteration-1-demo.firebaseio.com/')
-
-result = firebase.get('/1Ga1RxErjr9PRtH1UnTxMOHiSF4Ff30iBAc4vB_0IaC8', None)
-
-print(result)
